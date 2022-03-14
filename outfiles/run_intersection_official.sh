c786007c830657a0e1ea86e418f52414e8252f06
Amina Ly <amina.ly626@gmail.com>
2022-03-14 13:34:39 -0700

    update ml

--- outfiles/run_intersection_official.sh
+++ outfiles/run_intersection_official.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-user=aminaly@stanford.edu
 #SBATCH -p diffenbaugh
 
-ml curl physics gdal udunits/2.2.26 netcdf R/3.6.1 proj geos;
+ml curl physics gdal udunits netcdf proj geos R/4.1.2;
 
 cd $OAK/group_members/aminaly/mountain_biodiversity
 Rscript ./analysis/Intersection_Official.R
