0370ddb4a0e8032448fb0fbd5449b89f618b17a1
Amina Ly <amina.ly626@gmail.com>
2022-03-29 01:55:19 -0700

    if failure, just drop

--- analysis/Intersection_Official.R
+++ analysis/Intersection_Official.R
@@ -443,9 +443,9 @@ for (x in 1:length(listloop)){
                   }
                   
                   ovfprev <- ovfpol[ovfpol$STATUS_YR < year2, ]
-                  ovfprev <- 
                   ovfprev <- ovfprev %>% st_set_precision(1e5) %>% st_make_valid() 
-                  ovfprev3 <- tryCatch({ovfprev %>% st_union(by_feature = FALSE)}, error=function(e){print(paste("error ovfprev3:", e)); next}) #merge all polygons from previous years
+                  ovfprev3 <- tryCatch({ovfprev %>% st_union(by_feature = FALSE)}, error=function(e){print(paste("error ovfprev3:", e))}) #merge all polygons from previous years
+                  if(st_is_empty(ovfprev3)) next
                   if(PLOTIT){
                     plot(ovfprev3, add=T, col=w+2)
                   }
